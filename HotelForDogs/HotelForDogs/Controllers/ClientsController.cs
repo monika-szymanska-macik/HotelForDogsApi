6b617034f15e4385b80d3d6fec8bde48655532a9
Monika <m.szymanska.macik@gmail.com>
2021-02-24 11:07:53 +0100

    ClientController added

---
+++ HotelForDogs/HotelForDogs/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+﻿using HotelForDogs.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelForDogs.Controllers
+{
+    [ApiController]
+    public class ClientsController : ControllerBase 
+    {
+        private readonly IClientRepository _clientRepository;
+        public ClientsController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository ?? throw new ArgumentNullException(nameof(clientRepository));
+        }
+        [HttpGet("api/clients")]
+        public IActionResult GetClients()
+        {
+            var clientsFromRepo = _clientRepository.GetClients();
+            return new JsonResult(clientsFromRepo);
+        }
+    }
+}
